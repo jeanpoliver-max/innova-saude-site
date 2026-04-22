efa4d570253437a7d671c6beb001e1fd73257a5a
jeanpoliver-max <jeanp.oliver@gmail.com>
2026-04-22 13:32:00 -0300

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -153,7 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
-  base: '/innova-saude-site/',
+  base: '/',
   plugins,
   resolve: {
     alias: {
